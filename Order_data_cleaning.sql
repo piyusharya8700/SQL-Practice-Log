517bca626bc70a75314487466539fd48fa69012a
piyusharya8700 <piyusharya3003@proton.me>
2026-06-07 22:17:39 +0530

    Add orders data exploration and customer status standardization script

---
+++ Order_data_cleaning.sql
@@ -0,0 +1,27 @@
+select * from orders
+-- top 10 
+select * from orders limit 10
+-- select only customers info
+select customer_id, customer_status from orders
+-- select only gold customer info
+select * from orders where customer_status in ('Gold', 'GOLD')
+-- total orders done 
+select count(order_id) from orders
+-- total gross revenue
+select sum(total_retail_price_for_this_order) from orders
+-- which customer is our bigggest money maker
+select customer_status, sum(total_retail_price_for_this_order) from orders group by customer_status
+
+-- after running the upper query i came to know that the customer status consists of two types one
+--is Gold and other is GOLD same with every customer status
+
+update orders set customer_status='Gold' where customer_status='GOLD'
+-- lets try to make it less of repititive work
+update orders set customer_status=case
+when customer_status='PLATINUM' then 'Platinum'
+when customer_status='SILVER' then 'Silver'
+else customer_status
+end
+
+-- now lets check 
+select distinct customer_status from orders
