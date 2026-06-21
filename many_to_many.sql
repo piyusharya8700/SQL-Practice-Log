b69477c81e02cb8f7cc46dbc8019e12b435eaa1a
piyusharya8700 <piyusharya3003@proton.me>
2026-06-21 22:52:26 +0530

    Update many_to_many.sql

--- many_to_many.sql
+++ many_to_many.sql
@@ -19,6 +19,7 @@ foreign key(st_id) references studnts(student_id),
 foreign key(co_id) references coursera(course_id)
 )
 
+    
 insert into studnts(student_name, student_id)
 values('abc', 'st123'),
 ('def', 'st234'),
@@ -31,23 +32,23 @@ values('abc', 'st123'),
 ('y', 'st901'),
 ('z', 'st012')
 
-INSERT INTO coursera (course_name, course_id)
-VALUES 
-    ('cloud computing', 'cr123'),
-    ('course name', 'cr234'),
-    ('data science', 'cr345'),
-    ('cyber security', 'cr456'),
-    ('web dev', 'cr567'),
-    ('devops basics', 'cr678'),
-    ('ai models', 'cr789'),
-    ('mobile apps', 'cr890'),
-    ('ui ux design', 'cr901'),
-    ('game design', 'cr012');
+insert into coursera (course_name, course_id)
+values('cloud computing', 'cr123'),
+('course name', 'cr234'),
+('data science', 'cr345'),
+('cyber security', 'cr456'),
+('web dev', 'cr567'),
+('devops basics', 'cr678'),
+('ai models', 'cr789'),
+('mobile apps', 'cr890'),
+('ui ux design', 'cr901'),
+('game design', 'cr012');
 
 insert into junc(st_id, co_id)
 values ('st123', 'cr123'),
 ('st123', 'cr234'),
 ('st234', 'cr234'),
 ('st123', 'cr345')
+    
 
 select c.course_name, s.student_name from coursera c join junc j on c.course_id = j.co_id join studnts s on s.student_id = j.st_id
